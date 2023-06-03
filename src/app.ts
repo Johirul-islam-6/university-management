3e807a8dad91aba63573cfe3c0d53f6fae420507
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-03 13:39:31 +0600

    setup husky or lint-stage

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response, } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }))
 03. step- run system
 */
 
-app.get('/', (req : Request, res : Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
