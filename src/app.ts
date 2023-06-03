ee182d9aad2c4886acb41b21d8bf62ca94c04fbb
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-03 13:10:42 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, } from 'express'
+import express, { Application, Request, Response, } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: true }))
 03. step- run system
 */
 
-// app.get('/', (req : Request, res : any) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req : Request, res : Response) => {
+  res.send('Hello World!')
+})
 
 export default app
