c5895b85d69640369c9500b094d2fe07437e32f4
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-04 19:51:14 +0600

    Logger Winston set up complited

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose'
 import config from './config'
 import app from './app'
+import { errorLogger, logger } from './shared/logger'
 
 // database connected
 async function DataBase() {
-  console.log(config.databaser_url)
   try {
     await mongoose.connect(config.databaser_url as string)
-    console.log('📚 Database connected | All Ok')
+    logger.info('📚 Database connected | All Ok')
     app.listen(config.port, () => {
-      console.log(`Run Time listening on port ${config.port}`)
+      logger.info(`Run Time listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(`Database Connected fail ::`, error)
+    errorLogger.error(`Database Connected fail ::`, error)
   }
 }
 
