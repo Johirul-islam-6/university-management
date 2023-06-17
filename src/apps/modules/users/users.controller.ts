9615eee8a3e0a7265f18a15949b8ad57c02fac47
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-17 19:37:00 +0600

    set up UM-10 Branch zod validation error

--- src/apps/modules/users/users.controller.ts
+++ src/apps/modules/users/users.controller.ts
@@ -1,8 +1,21 @@
 import { RequestHandler } from 'express'
 import { UserServices } from './users.service'
+import { z } from 'zod'
 
 const singelUserCreated: RequestHandler = async (req, res, next) => {
   try {
+    // zod validation error setup
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'role is required',
+        }),
+        password: z.string().optional(),
+      }),
+    })
+
+    await createUserZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserServices.createdUser(user)
 
