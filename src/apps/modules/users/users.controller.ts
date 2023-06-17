2e0fb34d16c1cbb630e4fe376bef2bcffb161ae9
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-17 21:11:22 +0600

    setup successfully zod validation error

--- src/apps/modules/users/users.controller.ts
+++ src/apps/modules/users/users.controller.ts
@@ -1,34 +1,21 @@
-import { RequestHandler } from 'express'
-import { UserServices } from './users.service'
-import { z } from 'zod'
+import { RequestHandler } from 'express';
+import { UserServices } from './users.service';
 
 const singelUserCreated: RequestHandler = async (req, res, next) => {
   try {
-    // zod validation error setup
-    const createUserZodSchema = z.object({
-      body: z.object({
-        role: z.string({
-          required_error: 'role is required',
-        }),
-        password: z.string().optional(),
-      }),
-    })
-
-    await createUserZodSchema.parseAsync(req)
-
-    const { user } = req.body
-    const result = await UserServices.createdUser(user)
+    const { user } = req.body;
+    const result = await UserServices.createdUser(user);
 
     res.status(200).json({
       success: true,
       data: result,
       massage: 'user created successfully',
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const CreateUserController = {
   singelUserCreated,
-}
+};
