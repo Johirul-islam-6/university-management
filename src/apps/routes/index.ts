eebe092bc140857b288afe828e975ddf2764c2c8
Md.Johirule <johirulislam574206@gmail.comm>
2023-06-23 23:54:41 +0600

    academic faculty crud oparation add successfully

--- src/apps/routes/index.ts
+++ src/apps/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../modules/users/users.routes';
 import { SemesterRoute } from '../modules/academic_Semester/academicSemester.routes';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 const router = express.Router();
 
 const moduleRoutes = [
@@ -11,6 +12,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: SemesterRoute,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
   },
 ];
 
